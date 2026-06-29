f4cebe0895979c69a50ab76c671826a8173996a9
Deeksha Sharma <Deeksha@Abhijits-MacBook-Air.local>
2026-06-29 19:29:17 +0530

    added kub file and update base class with retry

--- src/pages/basePage.js
+++ src/pages/basePage.js
@@ -1,12 +1,34 @@
 const EnvManager = require('../config/envManager');
-class basePage {
+class BasePage {
     constructor(page) {
         this.page = page;
     }
 
     async launchApplication() {
         await this.page.goto(EnvManager.getBaseUrl());
+         if (!url) {
+            throw new Error('BASE_URL is missing. Please check .env, Jenkinsfile, Docker, or Kubernetes env.');
+        }
+
+        for (let attempt = 1; attempt <= 3; attempt++) {
+            try {
+                console.log(`Launching URL: ${url}, Attempt: ${attempt}`);
+                await this.page.goto(url, {
+                    waitUntil: 'domcontentloaded',
+                    timeout: 60000
+                });
+                return;
+            } catch (error) {
+                console.log(`Launch failed on attempt ${attempt}: ${error.message}`);
+
+                if (attempt === 3) {
+                    throw error;
+                }
+
+                await this.page.waitForTimeout(3000);
+            }
+        }    
     }
 }
 
-module.exports = basePage;
+module.exports = BasePage;
